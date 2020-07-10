9ed9897b1e150fae1d85fcad52a4eff8e8bc8c87
Icad <irsyadmuhammad.020201@gmail.com>
2020-07-10 20:08:58 +0700

    Singleton Design Pattern

---
+++ Singleton/Main.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        School newSchool = School.getInstance();
+        System.out.println(newSchool.toString());
+
+        // newSchool = new School();
+    }
+    
+}
